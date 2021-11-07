758f4454cad3cc31b2c83184f5802ead26114fac
Aditya <adityafav@gmail.com>
2021-11-07 13:57:59 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const router = express.Router();
+const { MongoClient } = require("mongodb");
+
+require("dotenv").config();
+
+const client = new MongoClient(process.env.MONGO_URL || "mongodb+srv://aditya:aditya@cluster0.3xhxi.mongodb.net");
+client.connect();
+const data = client.db("bluesky").collection("data");
+
+router.get("/countries", async (req, res, next) => {
+  let startYear = 1,
+    endYear = 3000;
+
+  if (req.query.startYear && parseInt(req.query.startYear) > 0) {
+    startYear = parseInt(req.query.startYear);
+  }
+
+  if (req.query.endYear && parseInt(req.query.endYear) > 0) {
+    endYear = parseInt(req.query.endYear);
+  }
+
+  data
+    .find({ year: { $gt: startYear - 1, $lt: endYear + 1 } })
+    .toArray((err, result) => {
+      if (err) throw err;
+
+      res.json(result);
+    });
+});
+
+router.get("/country/:category", async (req, res, next) => {
+  const match_strings = [];
+  let startYear = 1,
+    endYear = 3000;
+
+  if (req.query.startYear && parseInt(req.query.startYear) > 0) {
+    startYear = parseInt(req.query.startYear);
+  }
+
+  if (req.query.endYear && parseInt(req.query.endYear) > 0) {
+    endYear = parseInt(req.query.endYear);
+  }
+
+  const params = req.params.category.toUpperCase();
+
+  if (params.includes("CO2")) {
+    match_strings.push(/carbon_dioxide/);
+  }
+  if (params.includes("GHGS")) {
+    match_strings.push(/greenhouse/);
+  }
+  if (params.includes("CH4")) {
+    match_strings.push(/methane/);
+  }
+  if (params.includes("N2O")) {
+    match_strings.push(/nitrous_oxide/);
+  }
+  if (params.includes("SF6")) {
+    match_strings.push(/sulphur_hexafluoride/);
+  }
+
+  if (match_strings.length === 0) {
+    data
+      .find({ year: { $gt: startYear - 1, $lt: endYear + 1 } })
+      .toArray((err, result) => {
+        if (err) throw err;
+
+        res.json(result);
+      });
+  } else {
+    data
+      .find({
+        $and: [
+          { category: { $in: match_strings } },
+          { year: { $gt: startYear - 1, $lt: endYear + 1 } },
+        ],
+      })
+      .toArray((err, result) => {
+        if (err) throw err;
+
+        res.json(result);
+      });
+  }
+});
+
+module.exports = router;
